8f83fb74fa2a96633c96e843e26cb10121750b1d
kiran.yenigala@hmcts.net <Yrlks1091327~>
2020-08-04 23:21:25 +0100

    Add Scent

---
+++ src/com/lme/martian/Scent.java
@@ -0,0 +1,24 @@
+package com.lme.martian;
+
+import java.util.Objects;
+
+public class Scent {
+
+    private final Location location;
+
+    private final Orientation orientation;
+
+    public Scent(Location location, Orientation orientation) {
+        this.location = location;
+        this.orientation = orientation;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Scent scent = (Scent) o;
+        return Objects.equals(location, scent.location) &&
+                orientation == scent.orientation;
+    }
+}
