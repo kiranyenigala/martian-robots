f815fe22512d8864043d43559e408af0d4e3c9b7
kiran.yenigala@hmcts.net <Yrlks1091327~>
2020-08-04 23:53:52 +0100

    Add Planet behaviour

---
+++ src/com/lme/martian/Planet.java
@@ -0,0 +1,31 @@
+package com.lme.martian;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Planet {
+
+    private Location upperRightLocation;
+    private List<Robot> robots = new ArrayList<>();
+    private List<Scent> scents = new ArrayList<>();
+
+    public Planet(Location upperRightLocation) {
+        this.upperRightLocation = upperRightLocation;
+    }
+
+    public void addRobot(Robot robot) {
+        this.robots.add(robot);
+    }
+
+    public void addScent(Scent scent) {
+        this.scents.add(scent);
+    }
+
+    public Location getUpperRightLocation() {
+        return upperRightLocation;
+    }
+
+    public boolean hasScent(Scent tmpScent) {
+        return this.scents.stream().anyMatch(scent -> scent.equals(tmpScent));
+    }
+}
